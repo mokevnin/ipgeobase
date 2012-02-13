d1b6a88026a323d8cf6e697555c1f0c642993e84
Mokevnin Kirill <mokevnin@gmail.com>
2012-02-13 10:37:48 +0400

    add ip_meta_data

--- lib/ipgeobase/ip_meta_data.rb
+++ lib/ipgeobase/ip_meta_data.rb
@@ -1,3 +1,5 @@
+require 'iconv'
+
 module Ipgeobase
   class IpMetaData
     include HappyMapper
@@ -9,5 +11,17 @@ module Ipgeobase
     element :district, String, :deep => true
     element :lat, Float, :deep => true
     element :lng, Float, :deep => true
+
+    def city
+      Iconv.iconv('windows-1251', 'utf-8', @city).first
+    end
+
+    def country
+      Iconv.iconv('windows-1251', 'utf-8', @country).first
+    end
+
+    def region
+      Iconv.iconv('windows-1251', 'utf-8', @region).first
+    end
   end
 end
