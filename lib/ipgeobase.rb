bec863f359b613c468d1c563c7d8df3805c0aebc
Vasiliy <stiig@users.noreply.github.com>
2017-03-28 22:17:38 +0500

    Add ability to pass params into open-uri function

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -5,10 +5,10 @@ module Ipgeobase
   URL = 'http://ipgeobase.ru:7020/geo'
   autoload 'IpMetaData', 'ipgeobase/ip_meta_data'
 
-  def self.lookup(ip)
+  def self.lookup(ip, params = {})
     uri = URI.parse(URL)
     uri.query = URI.encode_www_form :ip => ip
-    resp = open(uri).read()
+    resp = open(uri, params).read()
     IpMetaData.parse(resp.to_s)
   end
 end
